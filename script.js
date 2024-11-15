54b82e805ab0914da8966408718ff993de1ef605
RenataGodoy <devrenatagodoy@gmail.com>
2024-11-15 01:46:53 -0300

    Add projects

--- script.js
+++ script.js
@@ -13,6 +13,13 @@ const projetos = [
         github: 'https://github.com/RenataGodoy/Petshop---ReactNative',
         demo: 'https://snack.expo.dev/czewM17jB2VBqeE-Nu57h'
     },
+    {
+        imagem: './Assets/Petshop-reactNative.png', 
+        linguagens: ['php', 'Laravel', 'bootstrap'],
+        nome: 'Ramal - Laravel',
+        github: 'https://github.com/RenataGodoy/Petshop---ReactNative',
+        demo: 'https://snack.expo.dev/czewM17jB2VBqeE-Nu57h'
+    },
 
 ];
 
@@ -24,8 +31,10 @@ const iconMap = {
     railway:   '<i class="devicon-railway-original-wordmark colored" linguagem-icon></i>',
     firebase: '<i class="devicon-firebase-plain-wordmark" linguagem-icon></i>',
     reactnative: '<i class="devicon-react-original-wordmark colored" linguagem-icon></i>',
-    
-    
+    php: '<i class="devicon-php-plain colored" linguagem-icon></i>',
+    laravel: '<i class="devicon-laravel-original-wordmark colored" linguagem-icon></i>',
+    bootstrap: '<i class="devicon-bootstrap-plain-wordmark colored" linguagem-icon></i>',
+
 
     
 
